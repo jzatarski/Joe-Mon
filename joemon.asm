32ba5068297132c701a595c65e103c58baffe1f0
Joe Zatarski <jzatar2@illinois.edu>
2015-12-16 19:59:51 -0600

    backspace support added backspace support in gets reorganized ram_version

--- joemon.asm
+++ joemon.asm
@@ -4,7 +4,7 @@
 ; This is written for use on a 68332 processor, but should be portable to
 ; other 68K archs without too much trouble.
 
-; Copyright 2015 Joe Zatarski
+; Copyright 2014,2015 Joe Zatarski
 ; email joe.zatarski@gmail.com
 
 ; TODO: put GPL notice here
@@ -14,18 +14,29 @@
 ; there is a ton of 68332 specific init code just at start. Be sure to change
 ; this to suit your system, and do any special init you need
 ; change rambase and ramsize for your system
+; the boot command sets the vector base register (VBR). Not all 68K
+; processors have this register.
+; to run software in user mode, the monitor creates a fake exception
+; frame on the stack and runs RTE. the format of this exception frame can
+; differ slightly between 68K variants.
+
+; if the symbol ram_version is defined, this is used as an indication that
+; the monitor should be assembled so that it can be downloaded into
+; RAM and tested without having to reburn EPROMs or flash memory ICs
 
 	ifnd ram_version
 rambase	equ	$400000	; this is where main RAM starts
-ramsize	equ	$100000	; 1MB in size
+ramend	equ	$500000	; one past end of RAM
 rombase	equ	$000000	; used for org
 
 	else
-rambase	equ	$5C0000
-ramsize	equ	$140000
+rambase	equ	monitor_end
+ramend	equ	$500000
 rombase	equ	$400000
 
 	endif
+
+ramsize	equ	ramend-rambase
 
 ; memory is organized as follows:
 ; top of RAM
@@ -33,7 +44,7 @@ rombase	equ	$400000
 ; monitor variables - 8K below top of RAM
 ; user stack
 ;
-super_stack	equ	rambase+ramsize	; stack is at end of RAM
+super_stack	equ	ramend	; stack is at end of RAM
 mon_ram		equ	super_stack-$2000	; start of variables used by the monitor
 user_stack	equ	mon_ram	; stack is predecrement, so this should be fine
 
@@ -46,10 +57,12 @@ sram2	equ	$600000	; start of battery backed SRAM card
 sram2size	equ	$100000	; 1MB in size
 
 ; a few character constants:
-newline	equ	$D
+newline	equ	$D	; most terminals send this when you hit enter
 cr	equ	$D
 lf	equ	$A
 esc	equ	$1B
+bs	equ	$8	; backspace character
+del	equ	$7F	; some terminals use delete in place of backspace
 
 	include "CPU.EQU"
 	include "MISC.EQU"
@@ -187,21 +200,20 @@ sysinit
 ; TODO: there are a couple simple RAM tests here as well, those should be moved
 ; to the main monitor code.
 
-	ifnd	ram_version
 	move.w	#$60C8,(SIMCR)	; set SIMCR such that:
-				; CLKOUT pin is	driven from an internal	clock
+				; CLKOUT pin is driven from an internal	clock
 				; source
 				; when FREEZE is asserted, software watchdog,
 				; periodic interupt timer counters, and bus
 				; monitor are disabled
 				; IMB is not available to an external master
-				; show internal	cycles disabled, external
+				; show internal cycles disabled, external
 				; arbitration enabled
 				; SIM regs restricted to supervisor access only
 				; internal modules mapped to $FF F000-$FF FFFF
 				; SIM IARB set to 8
 	move.w	#$7F00,(SYNCR)	; set SYNCR such that:
-				; clock	multiplier is 512 overall, system clock
+				; clock multiplier is 512 overall, system clock
 				; is about 16.8mhz
 				; ECLK is 1/8 system clock
 				; loss of crystal puts MCU in limp mode
@@ -216,18 +228,22 @@ sysinit
 				; base address of TPU RAM is $FF E000
 	move.w	#$140,(PITR) 	; set PITR such that:
 				; time period is approximately 1 second
-	move.w	#$540,(PICR)	; set PICR such	that:
+	move.w	#$540,(PICR)	; set PICR such that:
 				; use IRQ5
-				; vector number	#$40
-	move.w	#$35FF,(CSPAR0) ; set	CSPAR0 such that:
-				; CS5, CS2, CS1	and CSBOOT are 16 bit ports
+				; vector number #$40
+	move.w	#$35FF,(CSPAR0) ; set CSPAR0 such that:
+				; CS5, CS2, CS1 and CSBOOT are 16 bit ports
 				; CS4 is FC1
 				; CS3 is FC0
 	move.w	#$2B9,(CSPAR1)	; set CSPAR1 such that:
 				; CS8 is a 16 bit port
-				; CS10,	CS9, and CS7 are 8 bit ports
+				; CS10, CS9, and CS7 are 8 bit ports
 				; CS6 is ADDR19
-	move.w	#7,(CSBARBT)	; CSBARBT - CSBOOT base	addr at	$00 0000 with a	size of	1M
+
+; setting all this CS stuff *should* be OK for the RAM version as long as the location of the RAM we're loaded in
+; isn't actually changed here (it can be loaded again, with the same value, and it should be fine)
+
+	move.w	#7,(CSBARBT)	; CSBARBT - CSBOOT base addr at $00 0000 with a size of 1M
 	move.w	#$1007,(CSBAR0) ; CSBAR0 - CS0 base addr at $10 0000 with a size of 1M
 	move.w	#$6007,(CSBAR2) ; CSBAR2 - CS2 base addr at $60 0000 with a size of 1M
 	move.w	#$5C05,(CSBAR8) ; CSBAR8 - CS8 base addr at $5C 0000 with a size of 256K
@@ -236,14 +252,14 @@ sysinit
 	move.w	#$B007,(CSBAR7) ; CSBAR7 - CS7 base addr at $B0 0000 with a size of 1M
 	move.w	#$E000,(CSBAR9) ; CSBAR9 - CS9 base addr at $E0 0000 with a size of 2K
 	move.w	#$F003,(CSBAR10) ; CSBAR10 - CS10 base addr at $F0 0000 with a size of 64K
-	move.w	#$7870,(CSORBT) ; set	CSORBT for:
+	move.w	#$7870,(CSORBT) ; set CSORBT for:
 				; asynch mode
 				; assert on upper and lower bytes
 				; R/W access
 				; assert on address strobe
 				; 1 wait state
 				; supervisor/user space
-	move.w	#$7B70,(CSOR0)	; set CSOR0 for same as CSORBT	but:
+	move.w	#$7B70,(CSOR0)	; set CSOR0 for same as CSORBT but:
 				; 13 wait states
 	move.w	#$7B70,(CSOR2)	; set CSOR2 same as CSORBT but:
 				; 13 wait states
@@ -256,15 +272,15 @@ sysinit
 				; external DSACK
 	move.w	#$7B70,(CSOR9)	; set CSOR9 same as CSORBT but:
 				; 13 wait states
-	move.w	#$7B70,(CSOR10) ; set	CSOR10 same as CSORBT but:
+	move.w	#$7B70,(CSOR10) ; set CSOR10 same as CSORBT but:
 				; 13 wait states
 	move.w	#$8A,(QSMCR)	; set QSMCR for:
 				; QSM clock running
-				; ignore FREEZE	signal
+				; ignore FREEZE signal
 				; QSM regs supervisor only
 				; IARB number for QSM is 10
 	move.w	#0,(QTEST)	; QTEST
-	move.w	#$3050,(QILR)	; set QILR/QIVR	for:
+	move.w	#$3050,(QILR)	; set QILR/QIVR for:
 				; QSPI IRQ 6
 				; SCI IRQ disabled
 				; QSM interupt vector #$50
@@ -272,101 +288,101 @@ sysinit
 				; QSPI is a slave device
 				; outputs normal (not wired OR)
 				; 16 bits per transfer
-				; inactive state of SCK	is one
+				; inactive state of SCK is one
 				; data is changed on leading edge of SCK and captured on following edge
 				; baud generator disabled
-	move.b	#$70,(PORTQS)	; 'p' ; PORTQS - port QS set to output #$70
+	move.b	#$70,(PORTQS)	; PORTQS - port QS set to output #$70
 	move.w	#$A70,(PQSPAR)	; set PQSPAR/DDRQS such that:
 				; PQS 0, 4-6 are GPIO pins
-				; PQS 1	is MOSI
-				; PQS 3	is PCS0/SS
+				; PQS 1 is MOSI
+				; PQS 3 is PCS0/SS
 				; PQS 4-6 are outputs
 				; all others inputs or alternate function
 	move.w	#0,(SPCR3)	; set SPCR3/SPSR such that:
 				; QSPI loop mode off
-				; HALTA	and MODF interupts disabled
+				; HALTA and MODF interupts disabled
 				; halt not enabled
 	move.w	#$6084,(TPUMCR)	; set TPUMCR such that:
-				; TPU operating	normally (not stopped)
-				; timer	count register 1 clock is 1/256 system clock
-				; timer	count register 2 clock is external TCR2	clock source divided by	1
-				; TPU and TPURAM not in	emulation mode
+				; TPU operating normally (not stopped)
+				; timer count register 1 clock is 1/256 system clock
+				; timer count register 2 clock is external TCR2 clock source divided by 1
+				; TPU and TPURAM not in emulation mode
 				; all TPU registers supervisor only
 				; TPU IARB is set to 4
-	ori.w	#$DDDD,(CFSR3)	; set CFSR3 such that TPU channels 0-3	are set	for stepper motor control
-	ori.w	#$DDDD,(CFSR2)	; set CFSR2 such that TPU channels 4-7	are set	for stepper motor control
+	ori.w	#$DDDD,(CFSR3)	; set CFSR3 such that TPU channels 0-3	are set for stepper motor control
+	ori.w	#$DDDD,(CFSR2)	; set CFSR2 such that TPU channels 4-7	are set for stepper motor control
 	ori.w	#$98,(CFSR1)	; set CFSR1 such that
-				; TPU channel 9	is set for PWM
-				; TPU channel 8	is set for discrete input/output
+				; TPU channel 9 is set for PWM
+				; TPU channel 8 is set for discrete input/output
 	move.w	#$82,($FFFF00)	; set channel 0 such that:
 				; pin is low
-				; Capture TCR1,	Match TCR1
-	move.w	#$6666,($FFFF02) ;	sequence for CH0 to control a stepper motor
+				; Capture TCR1, Match TCR1
+	move.w	#$6666,($FFFF02)	; sequence for CH0 to control a stepper motor
 	move.w	#0,($FFFF04)	; stores current position of stepper motor
 	move.w	#0,($FFFF06)	; stores desired position of stepper motor
 	move.w	#1,($FFFF08)	; MOD_CNT and NEXT_STEP_RATE initialized as per documentation
-	move.w	#$101,($FFFF0A) ; only 1 step rate and last sec. channel is 1
+	move.w	#$101,($FFFF0A)	; only 1 step rate and last sec. channel is 1
 	move.w	#$82,($FFFF10)	; set channel 1 so that pin is low, capture TCR1, match TCR1
-	move.w	#$3333,($FFFF12) ; set pattern to control stepper motor
+	move.w	#$3333,($FFFF12)	; set pattern to control stepper motor
 	move.w	#0,($FFFF14)	; constant step rate
 	move.w	#0,($FFFF16)	; fastest possible step rate
 	move.w	#$82,($FFFF20)	; channel 2: pin is low, capture TCR1, match TCR1
-	move.w	#$6666,($FFFF22) ; pattern to control stepper motor
+	move.w	#$6666,($FFFF22)	; pattern to control stepper motor
 	move.w	#0,($FFFF24)	; initialize current position of stepper motor
 	move.w	#0,($FFFF26)	; init desired position of stepper motor
 	move.w	#1,($FFFF28)	; MOD_CNT and NEXT_STEP_RATE initialized as per docs
-	move.w	#$103,($FFFF2A) ; 1 step rate and last secondary channel is #3
+	move.w	#$103,($FFFF2A)	; 1 step rate and last secondary channel is #3
 	move.w	#$82,($FFFF30)	; set channel 3 so that pin is low, capture TCR1, match TCR1
-	move.w	#$3333,($FFFF32) ; set pattern to control stepper motor
+	move.w	#$3333,($FFFF32)	; set pattern to control stepper motor
 	move.w	#0,($FFFF34)	; constant step rate
 	move.w	#$DA7,($FFFF36)	; sets time between steps in TCR1 clocks
 	move.w	#$82,($FFFF40)	; set channel 4 so that pin is low, capture TCR1, match TCR1
-	move.w	#$3333,($FFFF42) ; set pattern to control stepper motor
+	move.w	#$3333,($FFFF42)	; set pattern to control stepper motor
 	move.w	#0,($FFFF44)	; init current position of the stepper motor
 	move.w	#0,($FFFF46)	; init desired position of stepper motor
-	move.w	#1,($FFFF48)	; MOD_CNT and	NEXT_STEP_RATE init as per docs
+	move.w	#1,($FFFF48)	; MOD_CNT and NEXT_STEP_RATE init as per docs
 	move.w	#$105,($FFFF4A)	; 1 step rate and last sec. channel is 5
 	move.w	#$82,($FFFF50)	; set channel 5 so that pin is low, capture TCR1, match TCR1
-	move.w	#$6666,($FFFF52) ; set pattern to control stepper motor
+	move.w	#$6666,($FFFF52)	; set pattern to control stepper motor
 	move.w	#0,($FFFF54)	; constant step rate
-	move.w	#$28F7,($FFFF56) ; sets time between steps	to #$28F7 TCR1 clocks
+	move.w	#$28F7,($FFFF56)	; sets time between steps to #$28F7 TCR1 clocks
 	move.w	#$82,($FFFF60)	; set channel 6 so that pin is low, capture TCR1, match TCR1
-	move.w	#$6666,($FFFF62) ; set pattern to control stepper motor
+	move.w	#$6666,($FFFF62)	; set pattern to control stepper motor
 	move.w	#0,($FFFF64)	; init current position of stepper motor
 	move.w	#0,($FFFF66)	; init desired position of stepper motor
 	move.w	#1,($FFFF68)	; MOD_CNT and NEXT_STEP_RATE initialized as per docs
 	move.w	#$107,($FFFF6A)	; 1 step rate and last secondary channel is 7
 	move.w	#$82,($FFFF70)	; set channel 7 so that pin is low, capture TCR1, match TCR1
-	move.w	#$3333,($FFFF72) ; set pattern to control stepper motor
+	move.w	#$3333,($FFFF72)	; set pattern to control stepper motor
 	move.w	#0,($FFFF74)	; constant step rate
 	move.w	#0,($FFFF76)	; no delay between steps
-	move.w	#$92,($FFFF90)	; 'ó' ; channel 9 is output, capture TCR1, compare TCR1, don't change PAC, force pin high
+	move.w	#$92,($FFFF90)	; channel 9 is output, capture TCR1, compare TCR1, don't change PAC, force pin high
 	move.w	#0,($FFFF94)	; set high time in TCR1 clocks
 	move.w	#149,($FFFF96)	; set period time in TCR1 clocks
 				; should be 440hz with about 50% duty cycle
-	move.w	#$AAAA,(HSRR1)	; init channel	0-7
+	move.w	#$AAAA,(HSRR1)	; init channel 0-7
 	move.w	#9,(HSRR0)	; init channel 9 and set channel 8 high
 	move.w	#$AAAA,(CPR1)	; priority set to middle for channel 0-7
-	move.w	#$A,(CPR0)	; priority set to middle for channels 8	and 9
+	move.w	#$A,(CPR0)	; priority set to middle for channels 8 and 9
 .tpuloop1:
 	cmpi.w	#0,(HSRR0)	; wait for TPU to finish what it's doing
 	bne.s	.tpuloop1
 .tpuloop2:
-	cmpi.w	#0,(HSRR1)	; still	waiting	for TPU
+	cmpi.w	#0,(HSRR1)	; still waiting for TPU
 	bne.s	.tpuloop2
-	move.w	#0,(CISR)	; clear	interupts
+	move.w	#0,(CISR)	; clear interupts
 	ori.w	#$800,(CFSR1)	; set CFSR1 such that TPU channel 10 is	discrete I/O
-	ori.w	#$10,(CPR0)	; channel 10 set to low	priority
+	ori.w	#$10,(CPR0)	; channel 10 set to low priority
 	ori.w	#$20,(HSRR0)	; set channel 10 output low
 	ori.w	#8,(CFSR0)	; set CFSR0 such that TPU channel 12 is	discrete I/O
-	ori.w	#$100,(CPR0)	; set channel 12 to low	priority
-	ori.w	#$100,(HSRR0)	; set channel 12 output	low
+	ori.w	#$100,(CPR0)	; set channel 12 to low priority
+	ori.w	#$100,(HSRR0)	; set channel 12 output low
 	move.w	#$F3,($FFFFE0)	; channel 14 is output, capture TCR2, compare TCR2, don't change PAC, force pin low
-	move.w	#$3D9,($FFFFE4) ; set time high in TCR2 clocks for channel 14
-	move.w	#$7B2,($FFFFE6) ; set period time in TCR2 clocks for channel 14
+	move.w	#$3D9,($FFFFE4)	; set time high in TCR2 clocks for channel 14
+	move.w	#$7B2,($FFFFE6)	; set period time in TCR2 clocks for channel 14
 	ori.w	#$900,(CFSR0)	; set channel 14 to PWM
 	ori.w	#$1000,(CPR0)	; low priority for channel 14
-	ori.w	#$2000,(HSRR0)	; init	channel	14
+	ori.w	#$2000,(HSRR0)	; init channel 14
 	move.w	#$FFFF,(DRAMC_CNTL) ; this register controls a few interupts from the PSU, as well as a couple pins on the DRAM controller
 	ori.w	#$8000,(CFSR0)	; set channel 15 to discrete I/O
 				; this pin controls the	/OE for	the DRAMC_CNTL latch
@@ -378,48 +394,55 @@ sysinit
 .tpuloop4:
 	cmpi.w	#0,(HSRR1)	; wait for TPU
 	bne.s	.tpuloop4
-	move.w	#0,(CISR)	; clear	TPU interupts
-	move.w	#$160,(TICR)	; set TICR such	that:
+	move.w	#0,(CISR)	; clear TPU interupts
+	move.w	#$160,(TICR)	; set TICR such that:
 				; TPU uses IRQ1
 				; vectors for TPU channels are #$6x
 	move.w	#$55,(CIER)	; set CIER such that interupts are enabled for channels 0, 2, 4, and 6
-	move.w	#0,(CISR)	; clear	interupts
+	move.w	#0,(CISR)	; clear interupts
 	move.b	#0,(MTR_CNTRL)	; U18 daughtercard
+
+	ifnd	ram_version	; can't reconfigure the DRAMC if we're in DRAM
+
 	move.w	#$EFFF,(DRAMC_CNTL) ; put /ML low (active) on the DRAM controller
 	move.w	#$CFFF,(DRAMC_CNTL) ; put /DISRFSH low (active) on the DRAM controller. This starts an external reset
 	move.l	#$200,d0	; load delay counter
 .dramloop1:
-	subi.l	#1,d0		; delay	for the	proper time of the reset
+	subi.l	#1,d0		; delay for the proper time of the reset
 	bne.s	.dramloop1
-	move.w	#$DFFF,(DRAMC_CNTL) ;	deassert /ML BEFORE /DISRFSH
-	move.w	#$FFFF,(DRAMC_CNTL) ;	now deassert /DISRFSH
-	movea.l	#%10001111001010011001100,a0 ; prepare a0 with programming info	for the	DRAM controller
-				; programming value for	DRAM controller:
+	move.w	#$DFFF,(DRAMC_CNTL) ; deassert /ML BEFORE /DISRFSH
+	move.w	#$FFFF,(DRAMC_CNTL) ; now deassert /DISRFSH
+	movea.l	#%10001111001010011001100,a0 ; prepare a0 with programming info for the DRAM controller
+				; programming value for DRAM controller:
 				; even address means /CAS outputs will be deasserted with /RAS outputs when /AREQ is deasserted
-				; even address means /WE output	functions normal, as write enable
-				; B1 is	forced high in hardware, so /ADS starts	memory access immediately
-				; B0 is	forced high in hardware, so row	col and	bank latches are fall through
-				; C9 is	forced high in hardware, so /CAS is delayed during WRITE accesses
-				; row address hold time	15ns min.
+				; even address means /WE output functions normal, as write enable
+				; B1 is forced high in hardware, so /ADS starts memory access immediately
+				; B0 is forced high in hardware, so row col and bank latches are fall through
+				; C9 is forced high in hardware, so /CAS is delayed during WRITE accesses
+				; row address hold time 15ns min.
 				; col. address setup time 0ns min.
-				; all RAS and CAS are selected during access, ECAS must	be asserted for	appropriate CAS	to be asserted
-				; B1 and B0 not	used
+				; all RAS and CAS are selected during access, ECAS must be asserted for appropriate CAS to be asserted
+				; B1 and B0 not used
 				; no error scrubbing
 				; 15 microsecond refresh period
 				; DELCLK divisor is 5
-				; RAS outputs all assert and negate at the same	time during refresh
+				; RAS outputs all assert and negate at the same time during refresh
 				; no address pipelining
-				; DTACK	output for /WAIT
-				; DTACK	will be	delayed	by one additional positive edge	of CLK (WAITIN is held active by ha
+				; DTACK output for /WAIT
+				; DTACK will be delayed by one additional positive edge of CLK (WAITIN is held active by ha
 	move.w	#$EFFF,(DRAMC_CNTL) ; assert /ML
-	move.w	(a0),d0		; write	programming info into the DRAM controller
+	move.w	(a0),d0		; write programming info into the DRAM controller
 	move.w	#$FFFF,(DRAMC_CNTL) ; deassert /ML
 	move.l	#$1FFFF,d0
 .dramloop2:
 	subi.l	#1,d0		; delay
 	bne.s	.dramloop2
 
+	endif
+
 	MOVE.W	#0,LEDS
+
+	ifnd ram_version	; skip ram test
 
 dramtest:
 	movea.l	#rambase,a0	; point	a0 at the beginning of DRAM
@@ -480,18 +503,15 @@ coldstart:
 	; interupts are assumed to be off at this point
 	; there will be a series of JSRs here to various hardware init
 	; not much yet, just the console for now
-	move.w	#$E000,(PPORT)
+
 	jsr	init_console
 	
 warmstart:
 	; this is after one-time hardware init
-	move.w	#$B000,(PPORT)
-	
+
 	jsr	clr_screen
-	move.w	#$9000,(PPORT)
 
 	jsr	init_mon_ram
-	move.w	#$8000,(PPORT)
 
 prompt_loop:	; loop which accepts and interprets monitor commands
 	movea.l	#.prompt,a0	; print the prompt
@@ -620,24 +640,49 @@ putc:
 gets:
 	; Get string
 	; gets characters until newline found or buffer filled
+	; backspace (^H) deletes previous character as does delete
+	; ignores characters with ascii value of less than #$20 except newline and BS
 	; echos characters as they are typed
 	; a0 holds pointer to buffer
 	; d0 gets trashed
 	; d1 holds buffer length (long)
-	; this routine always writes at LEAST 1 byte to the buffer
-	; even in the event the passed buffer length is 0
+	; d2 holds current character count
+	; 0 must not be passed as the buffer length, or it will be interpreted as 2^32
 	; calls getc which trashes other stuff (d0)
-	
-	subq.l	#1,d1	; subtract 1 FIRST because we need to reserve 1 location for the null
-	bls	.done	; branch if result was <= 0
+
+	clr.l	d2	; clear character counter
+	subq.l	#1,d1	; subtract 1 to reserve a spot for the null
+
+.next_char
 	jsr	getc
 	cmp.b	#newline,d0	; check newline char
 	beq	.done
+	cmp.b	#bs,d0	; check backspace
+	beq	.backspace
+	cmp.b	#del,d0	; check delete
+	beq	.backspace
+
+	addq.l	#1,d2	; add 1 to character counter
+	
 	move.b	d0,(a0)+	; add character to buffer
 	jsr	putc	; echo character
-	jmp	gets
+
+	cmp.l	d2,d1	; check if we've filled the buffer
+	bls	.done
+
+	jmp	.next_char
+
+.backspace
+	tst.l	d2	; check if we have any characters to delete
+	beq	.next_char
+
+	subq.l	#1,d2	; decrement character counter
+	suba	#1,a0	; move pointer back one character
+
+	jmp	.next_char
+
 .done
-	move.b	#0,(a0)+	; add null terminator
+	clr.b	(a0)+	; add null terminator
 	move.b	#cr,d0	; CR/LF to advance the line
 	jsr	putc
 	move.b	#lf,d0
@@ -996,7 +1041,9 @@ jump_cmd_string
 boot_cmd_string
 	dc.b	"BOOT",0
 	
-	
+	align	1
+monitor_end:	; this points just past the end of the monitor's code	
+
 input_buf_len	equ	32
 
 	offset mon_ram
